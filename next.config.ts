c29678d94dce56de8325fc84012e2261c77bab80
sourikduttanyu <sd5913@nyu.edu>
2025-06-14 21:34:58 -0400

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio"; // use "" if deploying to your root domain
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
